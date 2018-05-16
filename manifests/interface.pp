971451550516f571c7dacc33310a1049ca9a1468
Jordi Prats <jordi.prats@gmail.com>
2018-05-16 14:48:19 +0200

    intarface

---
+++ manifests/interface.pp
@@ -0,0 +1,27 @@
+define network::interface (
+                            $dev              = $name,
+                            $match            = false,
+                            $match_name       = undef,
+                            $match_macaddress = undef,
+                            $match_driver     = undef,
+                            $addresses        = [],
+                            $gateway4         = undef,
+                            $gateway6         = undef,
+                            $dns              = [],
+                            $dhcp4            = false,
+                            $dhcp6            = false,
+                            $optional         = false,
+                            $macaddress       = undef,
+                            $order            = '91',
+                          ) {
+  include ::netplan
+
+  file { "/etc/netplan/${91}-${dev}.yaml":
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template("${module_name}/inferface.erb"),
+  }
+
+}
