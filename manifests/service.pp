2e87b8e9ef72b7725745c754b4cfdbfb8cb0e3e8
Jordi Prats <jordi.prats@gmail.com>
2018-05-16 17:10:26 +0200

    neteja

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,12 +1,5 @@
 class netplan::service inherits netplan {
 
-  #
-  validate_bool($netplan::manage_docker_service)
-  validate_bool($netplan::manage_service)
-  validate_bool($netplan::service_enable)
-
-  validate_re($netplan::service_ensure, [ '^running$', '^stopped$' ], "Not a valid daemon status: ${netplan::service_ensure}")
-
   $is_docker_container_var=getvar('::eyp_docker_iscontainer')
   $is_docker_container=str2bool($is_docker_container_var)
 
