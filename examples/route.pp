30b444a8679f634c6595b9c7909e0ce91abf6a07
Jordi Prats <jordi.prats@gmail.com>
2018-05-17 11:35:40 +0200

    routes no dhcp

--- examples/route.pp
+++ examples/route.pp
@@ -1,9 +1,9 @@
 netplan::route { '69.69.69.0/24':
   gw  => '10.0.2.2/32',
-  dev => 'enp0s3',
+  dev => 'enp0s8',
 }
 
 netplan::route { '69.69.70.0/24':
   gw  => '10.0.2.2/32',
-  dev => 'enp0s3',
+  dev => 'enp0s8',
 }
