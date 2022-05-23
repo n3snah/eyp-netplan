872d7de5028291f50539b8dd2dd6b3012eba0112
Ross Hansen <hansen.ross@live.com.au>
2022-05-24 00:40:01 +0800

    feat: Adding additional unit tests to `netplan` class

--- spec/classes/netplan_spec.rb
+++ spec/classes/netplan_spec.rb
@@ -10,11 +10,16 @@ describe 'netplan' do
       it { is_expected.to compile.with_all_deps }
 
       it do
-        is_expected.to contain_class('netplan::install')
+        is_expected.to contain_class('netplan::install').with(
+          'manage_package' => true,
+          'package_ensure' => 'installed',
+        )
       end
 
       it do
-        is_expected.to contain_class('netplan::config')
+        is_expected.to contain_class('netplan::config').with(
+          'dir_purge' => false
+        )
       end
 
       it do
@@ -22,6 +27,132 @@ describe 'netplan' do
           'command' => '/usr/sbin/netplan apply',
           'refreshonly' => true,
         )
+      end
+
+      context 'with manage_package => true' do
+        let(:params) do
+          {
+            'manage_package' => true
+          }
+        end
+
+        it do
+          is_expected.to contain_class('netplan::install').with(
+            'manage_package' => true,
+            'package_ensure' => 'installed',
+          )
+        end
+
+        context 'with package_ensure => installed' do
+          let(:params) do
+            super().merge(
+              {
+                'package_ensure' => 'installed'
+              }
+            )
+          end
+  
+          it do
+            is_expected.to contain_class('netplan::install').with(
+              'manage_package' => true,
+              'package_ensure' => 'installed',
+            )
+          end
+        end
+
+        context 'with package_ensure => absent' do
+          let(:params) do
+            super().merge(
+              {
+                'package_ensure' => 'absent'
+              }
+            )
+          end
+  
+          it do
+            is_expected.to contain_class('netplan::install').with(
+              'manage_package' => true,
+              'package_ensure' => 'absent',
+            )
+          end
+        end
+      end
+
+      context 'with manage_package => false' do
+        let(:params) do
+          {
+            'manage_package' => false
+          }
+        end
+
+        it do
+          is_expected.to contain_class('netplan::install').with(
+            'manage_package' => false,
+            'package_ensure' => 'installed',
+          )
+        end
+
+        context 'with package_ensure => installed' do
+          let(:params) do
+            super().merge(
+              {
+                'package_ensure' => 'installed'
+              }
+            )
+          end
+
+          it do
+            is_expected.to contain_class('netplan::install').with(
+              'manage_package' => false,
+              'package_ensure' => 'installed',
+            )
+          end
+        end
+
+        context 'with package_ensure => absent' do
+          let(:params) do
+            super().merge(
+              {
+                'package_ensure' => 'absent'
+              }
+            )
+          end
+
+          it do
+            is_expected.to contain_class('netplan::install').with(
+              'manage_package' => false,
+              'package_ensure' => 'absent',
+            )
+          end
+        end
+      end
+
+      context 'with netplan_dir_purge => true' do
+        let(:params) do
+          {
+            'netplan_dir_purge' => true
+          }
+        end
+
+        it do
+          is_expected.to contain_class('netplan::config').with(
+            'dir_purge' => true
+          )
+        end
+      end
+
+      context 'with netplan_dir_purge => true' do
+        let(:params) do
+          {
+            'netplan_dir_purge' => false
+          }
+        end
+
+        it do
+          is_expected.to contain_class('netplan::config').with(
+            'dir_purge' => false
+          )
+        end
       end
     end
 
